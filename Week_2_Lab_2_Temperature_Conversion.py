7e0d4bb8453213a8075fe62e8623a28920a2f4cf
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-11 15:30:55 -0800

    Update

--- Week_2_Lab_2_Temperature_Conversion.py
+++ Week_2_Lab_2_Temperature_Conversion.py
@@ -2,17 +2,17 @@
 # CIS261
 # Temperature Conversion
 
-def to_celsius(fahrenheit):
+def toCelsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
-def to_fahrenheit(celsius):
+def toFahrenheit(celsius):
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
 
 if __name__ == "__main__":
     for temp in range(0, 212, 40):
-        print(temp, "Degrees Fahrenheit =", round(to_celsius(temp), 2), "Degrees Celsius")
+        print(temp, "Fahrenheit =", round(toCelsius(temp), 2), "Celsius")
 
     for temp in range(0, 100, 20):
-        print(temp, "Degrees Celsius =", round(to_fahrenheit(temp), 2), "Degrees Fahrenheit")
+        print(temp, "Celsius =", round(toFahrenheit(temp), 2), "Fahrenheit")
